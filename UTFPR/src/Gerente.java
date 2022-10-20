0ea0b9e20ca20756fd78622eb444c1fd09c3fc75
Alexandre Trieste <alexandre.trieste@gmail.com>
2022-10-20 13:22:13 -0300

    Feat: initial commit at Alexandre's bank

---
+++ UTFPR/src/Gerente.java
@@ -0,0 +1,19 @@
+public class Gerente {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private int senha;
+    private int numeroDeFuncionariosGerenciados;
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            System.out.println("Acesso Permitido!");
+            return true;
+        } else {
+            System.out.println("Acesso Negado!");
+            return false;
+        }
+    }
+
+    // outros métodos
+}
